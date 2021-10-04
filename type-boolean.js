0f239d77576574227dcb6c624b9c508693c37cec
lgcabrelli <39202202+lgcabrelli@users.noreply.github.com>
2021-10-04 18:59:40 -0300

    arquivos de teste
    testes de variáveis

---
+++ type-boolean.js
@@ -0,0 +1,7 @@
+const a = 'variavel A';
+const b = 'variavel B';
+const c = 'variavel A';
+
+console.log(a === b);
+console.log(a === c);
+
---
+++ type-number.js
@@ -0,0 +1,22 @@
+const meuNumero =3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+// console.log(operacaoMatematica);
+
+var a = 10
+var b = 0
+// console.log(a/b) // Infinity
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
